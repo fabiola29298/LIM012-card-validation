f459d6c5f67f042f2f7c761ff7a46344641201f8
fabiola29298 <fabiola29298@gmail.com>
2021-04-22 11:10:26 -0400

    js: agregando funcion de mostrar lista de productos desde json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-import validator from './validator.js';
+// import validator from './validator.js';
+// console.log(validator);
 
-console.log(validator);
+
+let listProducts = document.querySelector('.products');
+
+// show list of products from data.json
+function showProducts() {
+  listProducts.innerHTML =``;
+  const xhttp = new XMLHttpRequest();
+  xhttp.open('GET', 'data.json', true);
+  xhttp.send();
+  xhttp.onreadystatechange = function (){
+    if(this.readyState == 4 && this.status ==200){
+      let datos = JSON.parse(this.responseText);
+      for (let item of datos){
+        listProducts.innerHTML += `
+        <div class="container">
+          <img src="${item.url}" alt="">
+          <div class="info-product">
+            <input type="checkbox" name="product" value="${item.id}" />
+            <label for="send_newsletter">
+              <p class="item">${item.name}</p>
+              <p class="price"> ${item.price} Bs</p>
+            </label>
+          </div>
+        </div>
+        `;
+      }
+
+    }
+  }
+}
+showProducts();
